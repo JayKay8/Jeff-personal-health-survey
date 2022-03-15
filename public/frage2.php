4a45bd95a7be49f89e0c29f0fa25dc5bdc9d7b0a
JayKay8 <jeffrey.s@gmx.ch.com>
2022-03-15 17:01:58 +0100

    page 2 complete

---
+++ public/frage2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require "include/head.php" ?>
+</head>
+
+<body>
+    <header>
+        <?php include "include/header.php"; ?>
+    </header>
+    <main>
+    <div class="container">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-9">
+                    <!-- Text-->
+                    <form class="" id="question2">
+                        <h2>Frage 2:</h2><br>
+                        <h3>Nahrungsergänzungsmittel?</h3><br>
+                    <div class="text-fluid">
+
+                <!-- Image -->
+        <img src="/images/Nahrungsergänzung.png" class="img-fluid" alt="" />
+
+
+        <div class="question2">
+  <input class="form-check-input" type="radio" name="question2" id="flexRadioDefault1">
+  <label class="form-check-label" for="flexRadioDefault1">Ja</label>
+</div>
+        <div class="question2">
+  <input class="form-check-input" type="radio" name="question2" id="flexRadioDefault2">
+  <label class="form-check-label" for="flexRadioDefault2">Nein</label>
+  </div>
+  <button type="button" class="btn btn-primary">Absenden</button>
+
+
+    </div> 
+        
+
+    </main>
+    <footer>
+        <?php include "include/footer.php"; ?>
+    </footer>
+
+    <?php include "include/background.php"; ?>
+</body>
+
+
+
+<?php
--- public/index.php
+++ public/index.php
@@ -16,9 +16,9 @@
                 <div class="col-8">
                     <!-- Text-->
                     <form class="" id="intro">
-                        <h1>Umfrage zu "Ernährung, Fitness und Sport"</h1><br>
-                        <h2>10 Fragen zu deinem Körperlichen Wohlbefinden</h2><br>
-                        <h3>Willkommen zu unserem Fragebogen!</h3>
+                        <h3>Umfrage zu "Ernährung, Fitness und Sport"</h3><br>
+                        <h4>10 Fragen zu deinem Körperlichen Wohlbefinden</h4><br>
+                        <h4>Willkommen zu unserem Fragebogen!</h4>
                         <div class="text-center">
 
                 <!-- Image -->
@@ -32,7 +32,7 @@
                     <form class="" id="intro">
                         <h4>Nehmen sie sich ein paar Minuten Zeit um die Fragen zu beantworten</h4><br>
                         <h4>Beginnen wir mit Frage 1</h4><br>
-                        <h2>Wie Gesund bist du?</h2>
+                        <h3>Wie Gesund bist du?</h3>
                     <div class="text-center">
                 </div>                              
             </div>
@@ -40,7 +40,7 @@
         
         <label for="question1" class="form-label">Bewertung 1 - 5</label>
     <input type="range" class="form-range" min="0" max="5" step="1" id="question1">  
-    <button type="button" class="btn btn-primary">Absenden</button>  
+    <button type="button" class="btn btn-primary">Absenden</button><br> 
 
     </main>
     <footer>
