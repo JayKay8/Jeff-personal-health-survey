846de2ab7137bb028776f6d4035edb449db4cbbb
JayKay8 <jeffrey.s@gmx.ch.com>
2022-03-15 15:34:31 +0100

    main page done

--- public/include/head.php
+++ public/include/head.php
@@ -4,7 +4,8 @@
     <title>Personal Health Survey</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="background.css">
+    <link rel="stylesheet" href="app.css">
     <script defer src="https://pro.fontawesome.com/releases/v5.10.0/js/all.js"
         integrity="sha384-G/ZR3ntz68JZrH4pfPJyRbjW+c0+ojii5f+GYiYwldYU69A+Ejat6yIfLSxljXxD"
         crossorigin="anonymous"></script>
+    <script src="app.js" defer></script>
--- public/index.php
+++ public/index.php
@@ -14,15 +14,33 @@
             <div class="row">
                 <div class="col-2"></div>
                 <div class="col-8">
-                    <!-- form goes here-->
+                    <!-- Text-->
                     <form class="" id="intro">
                         <h1>Umfrage zu "Ernährung, Fitness und Sport"</h1><br>
                         <h2>10 Fragen zu deinem Körperlichen Wohlbefinden</h2><br>
                         <h3>Willkommen zu unserem Fragebogen!</h3>
                         <div class="text-center">
+
+                <!-- Image -->
         <img src="/images/health.jpg" class="img-fluid" alt="" />
+
+        <div class="container">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <!-- Text-->
+                    <form class="" id="intro">
+                        <h4>Nehmen sie sich ein paar Minuten Zeit um die Fragen zu beantworten</h4><br>
+                        <h4>Beginnen wir mit Frage 1</h4><br>
+                        <h2>Wie Gesund bist du?</h2>
+                    <div class="text-center">
+                </div>                              
             </div>
-                </div>          
+        </div> 
+        
+        <label for="question1" class="form-label">Bewertung 1 - 5</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="question1">  
+    <button type="button" class="btn btn-primary">Absenden</button>  
 
     </main>
     <footer>
